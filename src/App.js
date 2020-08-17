6c59adb9bb93330f9141e6c138ebe87ac40cf75b
Mike <mmmcounts@gmail.com>
2020-08-17 16:00:27 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './components/game/Game';
+import renegadesLogo from './assets/images/ralph.jpg';
+import outlawsLogo from './assets/images/bob.jpg';
+import piratesLogo from './assets/images/michael.jpg';
+import buccaneersLogo from './assets/images/tom.jpg'
+//need to import more things, look at Davey's App
+//need to change what it returns, don't get rid of all of this
+//import whichever component/s App uses and also the images go here
 
-function App() {
+
+function App(props) {
+  const renegades = {
+    name: 'Renegades',
+    logoSrc: renegadesLogo
+  }
+
+  const outlaws = {
+    name: 'Outlaws',
+    logoSrc: outlawsLogo
+  }
+
+  const pirates = {
+    name: 'Pirates',
+    logoSrc: piratesLogo
+  }
+
+  const buccaneers = {
+    name: 'Buccaneers',
+    logoSrc: buccaneersLogo
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Ralph Wiggum's Playhouse"
+        homeTeam={renegades}
+        visitingTeam={outlaws}
+      />
+      <Game
+        venue="Sideshow Bob's Prison Cell"
+        homeTeam={outlaws}
+        visitingTeam={renegades}
+      />
+      <Game
+        venue="The Lonely Island"
+        homeTeam={pirates}
+        visitingTeam={buccaneers}
+      />
+      <Game
+        venue="Matt Damon's Basement"
+        homeTeam={buccaneers}
+        visitingTeam={pirates}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/team/Team.js
@@ -0,0 +1,39 @@
+//Both Scoreboard and Team are functional components that, in this instance, does not need anything extra other than React boilerplate.
+
+import React from 'react';
+function Team(props) {
+    let shotPercentageDiv
+
+    if (props.stats.shots) {
+        const shotPercentage = Math.round((props.stats.score / props.stats.shots) * 100)
+        shotPercentageDiv = (
+            <div>
+                <strong>Shooting %: {shotPercentage}</strong>
+            </div>
+        )
+    }
+
+    return (
+        <div className="Team">
+            <h2>{props.name}</h2>
+
+            <div className="identity">
+                <img src={props.logo} alt={props.name} />
+            </div>
+
+            <div>
+                <strong>Shots:</strong> {props.stats.shots}
+            </div>
+
+            <div>
+                <strong>Score:</strong> {props.stats.score}
+            </div>
+
+            {shotPercentageDiv}
+
+            <button onClick={props.shotHandler}>Shoot!</button>
+        </div>
+    )
+}
+
+export default Team
